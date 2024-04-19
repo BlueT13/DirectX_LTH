ae3bd825ff57c3a8b152c169e3fe2624a8cb2c0a
TaeHwan <alex23456@naver.com>
2024-04-19 16:21:03 +0900

    [Feature] 생성한 TileMap 데이터 PlayLevel에서 Load 구현

--- Dungreed/Background.cpp
+++ Dungreed/Background.cpp
@@ -1,10 +1,11 @@
 #include "PreCompile.h"
 #include "Background.h"
+#include <EngineCore/TileRenderer.h>
 
 ABackground::ABackground()
 {
-	Renderer = CreateDefaultSubObject<USpriteRenderer>("Renderer");
-	SetRoot(Renderer);
+	TileRenderer = CreateDefaultSubObject<UTileRenderer>("TileRenderer");
+	SetRoot(TileRenderer);
 }
 
 ABackground::~ABackground()
@@ -15,8 +16,27 @@ void ABackground::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Renderer->SetSprite("Town.png");
-	Renderer->SetAutoSize(UDungreedConstValue::AutoSize, true);
+	//Renderer->SetSprite("Town.png");
+	//Renderer->SetAutoSize(UDungreedConstValue::AutoSize, true);
+
+	UEngineSerializer Ser;
+
+	Dir.MoveToSearchChild("Resources");
+	Dir.Move("Save");
+	UEngineFile File = Dir.GetPathFromFile("SaveData.Data");
+	File.Open(EIOOpenMode::Read, EIODataType::Binary);
+	File.Load(Ser);
+
+	Ser >> TilesData;
+	TileRenderer->CreateTileMap("Tiles.png", { 64.0f, 64.0f }, TilesData.size(), TilesData[0].size(), 0);
+
+	for (size_t y = 0; y < TilesData.size(); y++)
+	{
+		for (size_t x = 0; x < TilesData[y].size(); x++)
+		{
+			TileRenderer->SetTile(x, y, TilesData[y][x]);
+		}
+	}
 }
 
 void ABackground::Tick(float _DeltaTime)
--- Dungreed/Background.h
+++ Dungreed/Background.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <EngineCore/Actor.h>
 #include <EngineCore/SpriteRenderer.h>
+#include <EngineBase/EngineDirectory.h>
+#include <EngineCore/TileRenderer.h>
 
 // Ό³Έν :
 class ABackground : public AActor
@@ -23,7 +25,10 @@ protected:
 	void Tick(float _DeltaTime) override;
 
 private:
-	USpriteRenderer* Renderer;
+	//USpriteRenderer* Renderer;
 
+	UTileRenderer* TileRenderer = nullptr;
+	UEngineDirectory Dir;
+	std::vector<std::vector<int>> TilesData;
 };
 
